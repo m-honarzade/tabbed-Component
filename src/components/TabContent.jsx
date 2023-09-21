f1134dee32cbfed68c4562e137a3ba157395c71c
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-09-21 13:52:51 +0430

    add button Functionality

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -5,6 +5,13 @@ const TabContent = ({ content }) => {
   console.log(content);
   const [like, setlike] = useState(0);
   const [showDetails, setShowDetails] = useState(true);
+  const undoHandler = () => {
+    setlike(0);
+    setShowDetails(true);
+  };
+  const undoLaterHandler = () => {
+    setTimeout(undoHandler, 2000);
+  };
   return (
     <Card>
       <div className="flex flex-col gap-y-3 px-3 py-5">
@@ -27,19 +34,31 @@ const TabContent = ({ content }) => {
           <div className="flex flex-row gap-1 text-sm justify-center items-center">
             <span className="text-sm">{like}</span>
             <span className="text-[#f03e3e] text-2xl">♥</span>
-            <button className="bg-[#f03e3e] rounded-md px-2 py-0 text-white ">
+            <button
+              onClick={() => setlike((prev) => prev + 1)}
+              className="bg-[#f03e3e] rounded-md px-2 py-0 text-white "
+            >
               +
             </button>
-            <button className="bg-[#f03e3e] rounded-md px-2 py-0 text-white ">
+            <button
+              onClick={() => setlike((prev) => prev + 3)}
+              className="bg-[#f03e3e] rounded-md px-2 py-0 text-white "
+            >
               +++
             </button>
           </div>
         </div>
         <div className="flex flex-row gap-x-1">
-          <button className="bg-[#364fc7] text-white text-sm  rounded-md px-3 py-1">
+          <button
+            onClick={undoHandler}
+            className="bg-[#364fc7] text-white text-sm  rounded-md px-3 py-1"
+          >
             Undo
           </button>
-          <button className="bg-[#364fc7] text-white text-sm  rounded-md px-3 py-1">
+          <button
+            onClick={undoLaterHandler}
+            className="bg-[#364fc7] text-white text-sm  rounded-md px-3 py-1"
+          >
             Undo in 2s
           </button>
         </div>
